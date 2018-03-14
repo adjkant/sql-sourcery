bd575eb3f9387ccaad05a1fff1252b9117ba0011
Adrian Kant <adjkant@gmail.com>
2018-03-14 15:32:24 -0400

    connection and table creation, gitignore changes

--- lib/lang-example.rkt
+++ lib/lang-example.rkt
@@ -1,2 +1,7 @@
 #lang s-exp "lang.rkt"
 
+(sourcery-db "test.db")
+(sourcery-struct user [(name INT)])
+
+1
+2
--- lib/lang.rkt
+++ lib/lang.rkt
@@ -1,7 +1,107 @@
 #lang racket
 
 (provide
- #%module-begin
+ (rename-out (sourcery-begin #%module-begin))
  #%app
  #%datum
- #%top-interaction)
+ #%top
+ #%top-interaction
+ sourcery-db
+ sourcery-struct
+ define)
+
+;; Language Requirements
+(require db
+         (for-syntax syntax/parse
+                     racket
+                     db))
+
+
+;; Confirm Program Shape
+(define-syntax sourcery-begin
+  (syntax-parser
+    [(_ ((~literal sourcery-db) path:string)
+        ((~literal sourcery-struct) name fields) ...
+        prog ...)
+     (if (sqlite3-available?)
+         #`(#%module-begin
+            (sourcery-db path)
+            (sourcery-struct name fields) ...
+            prog ...)
+         #`(#%module-begin (error 'sqllite3 "SQLite 3 is not available on this system.")))]))
+
+
+;; Ensure Existence of Database
+(define-syntax sourcery-db
+  (syntax-parser
+    [(_ path:string)
+     #'(set-conn!
+         (sqlite3-connect
+          #:database path
+          #:mode 'create))]))
+
+;; Ensure Table Existence
+(define-syntax sourcery-struct
+  (syntax-parser
+    [(_ struct-name:id [(field type) ...])
+     (let
+         [(creation-string (table-creation-string #'struct-name #'(field ...) #'(type ...)))]
+       #`(query-exec sourcery-connection #,creation-string))]))
+
+;; -----------------------------------------------------------------------
+;; SQL CREATE Generation
+
+;; Syntax Syntax Syntax -> String
+;; Given the name of the structure and the fields/types, create a table creation string
+(define-for-syntax (table-creation-string struct-name fields types)
+  (let [(fields-decs (table-field-declarations fields types))]
+    (string-append
+     "CREATE TABLE IF NOT EXISTS"
+     " "
+     (id->string struct-name)
+     "("
+     (substring fields-decs 0 (- (string-length fields-decs) 2))
+     ")")))
+
+;; Syntax Syntax -> String
+;; take the fields and types syntax objects and convert to a creation string for fields in a SQL table
+(define-for-syntax (table-field-declarations fields types)
+  (foldl
+   (λ (col-def so-far)
+     (string-append
+      col-def
+      ", "
+      so-far))
+   ""
+   (map (λ (ft-pair)
+          (string-append
+           (id->string (first ft-pair))
+           " "
+           (sourcery-t->db-t (id->string (second ft-pair)))))
+        (map list (syntax->list fields) (syntax->list types)))))
+
+
+;; String -> String
+;; convert a sourcery type to a database type
+(define-for-syntax (sourcery-t->db-t external-type-string)
+  external-type-string)
+
+;; -----------------------------------------------------------------------
+;; Phase 1 Utilities
+
+;; ID to String
+;; Convert an identifier to a string
+(define-for-syntax (id->string id)
+  (symbol->string `,(syntax->datum id)))  
+
+;; -----------------------------------------------------------------------
+;; Sourcery Connection Runtime Library
+
+;; The single database connection for a SQLSourcery program
+(define sourcery-connection #f)
+
+;; Set the connection
+(define (set-conn! conn)
+  (begin
+    (set! sourcery-connection conn)
+    (void)))
