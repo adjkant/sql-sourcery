abb94e11b6cc835cc4fbd1bbfdba935e1c7ba63b
taylorcmurphy <taylor.connell.murphy@gmail.com>
2018-03-18 14:28:59 -0400

    added multi-filed struct to example code

--- lib/lang-example.rkt
+++ lib/lang-example.rkt
@@ -2,7 +2,10 @@
 
 (sourcery-db "test.db")
 (sourcery-struct user [(name INTEGER)])
-
+(sourcery-struct users [(name INTEGER) (grade INTEGER)])
+(user-create -1)
+(users-create 3 4)
+(users-create 2 3)
 1
 2
 
